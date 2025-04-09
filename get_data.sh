22975091c25946bead65b51aa835421ea81c5a0c
Jaime Torres <156240086+mews6@users.noreply.github.com>
2025-04-09 13:05:23 -0500

    add minimal boilerplate files

---
+++ get_data.sh
@@ -0,0 +1,8 @@
+mkdir data
+cd data
+
+echo "Downloading Images..."
+curl -L -o ~/Downloads/gastrointestinal-bleeding-images-dataset.zip\
+  https://www.kaggle.com/api/v1/datasets/download/aryashah2k/gastrointestinal-bleeding-images-dataset
+
+..
