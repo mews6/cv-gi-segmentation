77d24862c14f63efc9eb85afd068a5022a269e76
Jaime Torres <jatber.mrowr@gmail.com>
2025-04-28 13:26:40 -0500

    change data requirements

--- get_data.sh
+++ get_data.sh
@@ -1,8 +1,9 @@
 mkdir data
-cd data
 
 echo "Downloading Images..."
-curl -L -o ~/Downloads/gastrointestinal-bleeding-images-dataset.zip\
+#!/bin/bash
+curl -L -o ./data/gastrointestinal-bleeding-images-dataset.zip\
   https://www.kaggle.com/api/v1/datasets/download/aryashah2k/gastrointestinal-bleeding-images-dataset
-
-..
+cd data
+echo "Unzipping Images..."
+unzip -q gastrointestinal-bleeding-images-dataset.zip
