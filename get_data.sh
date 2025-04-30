3a1820a7dc01d3dac10b90a628d708ca82be255a
Jaime Torres <jatber.mrowr@gmail.com>
2025-04-30 13:00:20 -0500

    add a second model + refactor

--- get_data.sh
+++ get_data.sh
@@ -1,10 +1,20 @@
 mkdir data
 
-echo "Downloading Images..."
+echo "Downloading Blood Classification Images..."
 #!/bin/bash
 curl -L -o ./data/gastrointestinal-bleeding-images-dataset.zip\
   https://www.kaggle.com/api/v1/datasets/download/aryashah2k/gastrointestinal-bleeding-images-dataset
 cd data
 echo "Unzipping Images..."
 unzip -q gastrointestinal-bleeding-images-dataset.zip
+echo "Unzipped!"
+cd..
+
+echo "Downloading Segmentation"
+#!/bin/bash
+curl -L -o ./data/kvasir-dataset-for-classification-and-segmentation.zip\
+  https://www.kaggle.com/api/v1/datasets/download/abdallahwagih/kvasir-dataset-for-classification-and-segmentation
+cd data
+echo "Unzipping Images..."
+unzip -q kvasir-dataset-for-classification-and-segmentation.zip
 echo "Unzipped!"
