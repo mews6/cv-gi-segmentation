f6980a033d6bd823e755c7398b49183083f3ea27
Jaime Torres <156240086+mews6@users.noreply.github.com>
2025-05-13 20:08:47 -0500

    add camera opener

--- src/main.py
+++ src/main.py
@@ -1,6 +1,33 @@
-import torch
-from torch.utils.data import Dataset
-from torchvision import datasets
-from torchvision.transforms import ToTensor
-import matplotlib.pyplot as plt
+import numpy as np
+import cv2 as cv
+ 
 
+def open_camera():
+    cap = cv.VideoCapture(0)
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+    
+        # if frame is read correctly ret is True
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        # Our operations on the frame come here
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        # Display the resulting frame
+        cv.imshow('frame', gray)
+        if cv.waitKey(1) == ord('q'):
+            break
+    
+    # When everything done, release the capture
+    cap.release()
+    cv.destroyAllWindows()
+
+def main():
+    open_camera()
+
+if __name__ == main:
+    main()
